7b119c8e8b4f084fd74c0663af48022b8dc7f1da
bluesenai <bluesenai1910@gmail.com>
2026-02-06 10:49:55 -0300

    termino da fase 3

---
+++ src/main/java/br/dev/blue/OsApiApplication/domain/repository/ClienteRepository.java
@@ -0,0 +1,14 @@
+
+package br.dev.blue.OsApiApplication.domain.repository;
+
+import br.dev.blue.OsApiApplication.domain.model.Cliente;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    
+    List<Cliente> findByNome(String nome);
+    List<Cliente> findByNomeContaining(String nome);
+}
