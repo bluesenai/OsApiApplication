5aad6ed0b7b9fac446d50bcfc4572c7ebc206064
bluesenai <bluesenai1910@gmail.com>
2026-02-20 07:57:23 -0300

    termino da fase 5 (sem correção)

--- src/main/java/br/dev/blue/OsApiApplication/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/dev/blue/OsApiApplication/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,9 +1,46 @@
 
 package br.dev.blue.OsApiApplication.api.exceptionhandler;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import static org.springframework.web.servlet.function.RequestPredicates.headers;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid {
+       MethodArgumentNotValidException ex, 
+        HttpHeaders headers, 
+        HttpStatus status, 
+        WebRequest request) {
+
+    ProblemaException problema = new ProblemaException();
+    problema.setStatus(status.value());
+    problema.setTitulo("Um ou mais campos inválidos! Tente novamente.");
+    problema.setDataHora(LocalDateTime.now());
     
+    List<ProblemaException.CampoProblema(nomeCampo, mensagemCampo));
+    
+    for(ObjectError error : ex.getBindingResult().getAllErrors()) {
+        String nomeCampo = ((FieldError) error).getField();
+        String mensagemCampo = error.getDefaultMessage();
+       
+        camposComErro.add(new ProblemaException.CampoProblema(nomeCampo, mensagemCampo));
+    }
+    problema.setCampos(camposComErro);
+        return super.handleMethodArgumentNotValid(ex, headers, status, request); 
+    
+        }
+    }
 }
--- src/main/java/br/dev/blue/OsApiApplication/api/exceptionhandler/ProblemaException.java
+++ src/main/java/br/dev/blue/OsApiApplication/api/exceptionhandler/ProblemaException.java
@@ -1,12 +1,22 @@
 
 package br.dev.blue.OsApiApplication.api.exceptionhandler;
 
+import java.util.List;
+
 
 public class ProblemaException {
    private Integer status;
    private LocaDateTime dataHora;
    private String titulo;
 
+   private List<CampoProblema> listaCamposProblema;
+   
+   public List<CampoProblema> getCampos() {
+       return listaCamposProblema;
+   }
+   public void setCampos(List<CampoProblema> campos) {
+       this.listaCamposProblema = campos;
+   }
     public Integer getStatus() {
         return status;
     }
@@ -31,4 +41,25 @@ public class ProblemaException {
         this.titulo = titulo;
     }
    
+    public static class CampoProblema {
+        private String nomeCampo;
+        private String mensagemCampo;
+        
+        public CampoProblema(String nomeCampo, String mensagemCampo) {
+            this.nomeCampo = nomeCampo;
+            this.mensagemCampo = mensagemCampo;
+        }
+        public String getNomeCampo() {
+            return nomeCampo;
+        }
+        public void setNomeCampo(String nomeCampo){
+            this.nomeCampo = nomeCampo;
+        }
+        public String getMensagemCampo() {
+            return mensagemCampo;
+        }
+        public void setMensagemCampo(String mensagemCampo) {
+            this.mensagemCampo = mensagemCampo;
+        }
+    }
 }
